5e41490ad4f34d86ab1f938716cfd32f32f29890
ASinamet <alfredsinameta00@gmail.com>
2022-09-26 15:22:16 +0200

    gg

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const WebSocket = require('ws')
 
 const wss = new WebSocket.Server({ port: 8080 })
+console.log('Server started on port 8080');
 wss.on('connection', (ws) => {
   ws.on('message', message => {
     console.log(`Received message => ${message}`)
