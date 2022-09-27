41f0d28b7082a3ab17aaaf24973a6ee7de6085e6
ASinamet <alfredsinameta00@gmail.com>
2022-09-27 19:49:06 +0200

    added_git_ignore

--- client/index.js
+++ client/index.js
@@ -1,20 +1,30 @@
 const WebSocket = require('ws');
-const readline = require('prompt-sync')({sigint: true});
+const readline = require('readline');
+const { stdin, stdout } = require('process');
+const rl = readline.createInterface({
+	input: stdin,
+	output: stdout,
+	terminal: true,
+	prompt: '>'
+});
 
 const url = 'ws://localhost:8080'
 const client = new WebSocket(url)
 
+//Web socket Callbacks setting
+
 client.on('open', () => {
 	console.log('Connected To Server!');
+	console.log('--Write "exit" to quit--');
 });
 
 client.on('error', (e) => {
 	console.log(`WebSocket error: ${e}`);
-  process.exit();
+	process.exit();
 });
 
 client.on('message', (mess) => {
-	console.log('SERVER SAID ===>  ' + mess)
+	console.log('SERVER SAID ===>	' + mess)
 });
 
 client.on('close', () => {
@@ -22,23 +32,40 @@ client.on('close', () => {
 	process.exit();
 });
 
-console.log('--All is printed by server--');
-console.log('--Write "exit" to quit--');
+
+//Terminal input control
+
+rl.on('line', (line) => {
+	line = line.trim().toString();
+	if ( line.length > 0 && client.readyState == client.OPEN){
+		client.send(line);
+	}
+	else if (line == 'exit')
+		rl.close();
+	//if (client.readyState == client.OPEN)
+})
+
+rl.on('close', () => {
+	console.log('\nBye Bye!!');
+	process.exit();
+});
+
+//ADD LITTLE EVENT LOOP TO CONSENT CLIENT-LIKE USAGE
 
 // if (client.readyState != client.OPEN)
 // {
-// 	console.log('No server!!');
-// 	process.exit();
+//	 console.log('No server!!');
+//	 process.exit();
 // }
-while (1)
-{
-  let inp = readline(">");
-  if (inp == 'exit') {
-	client.close(200);
-	console.log('You logged out!!');
-	process.exit();
-  }
-  else if (inp.length > 1) {
-    client.send(inp);
-  }
-}
+// while (1)
+// {
+//	 let inp = readline(">");
+//	 if (inp == 'exit') {
+//	 client.close(200);
+//	 console.log('You logged out!!');
+//	 process.exit();
+//	 }
+//	 else if (inp.length > 1) {
+//		 client.send(inp);
+//	 }
+// }
--- server/index.js
+++ server/index.js
@@ -3,26 +3,29 @@ const WebSocketServer = require('ws').Server
 const wss = new WebSocketServer({ port: 8080})
 console.log('Server started on port 8080');
 wss.on('connection', (ws) => {
-  ws.on('open' ,(client) => {
-    console.log('New client connected!');
-    client.send('Hello new client!');
-  })
-  ws.on('message', (message) => {
-    console.log(`Received message => ${message}`);
-  });
-  //Close server after first disc
-  ws.on('close', () => {
-    console.log('Some Client Closed Connection!');
-  });
-  ws.on('error', (err) => {
-	console.log(`WebSocket Error: ${err.message}`)
-  })
+	ws.on('open' ,(client) => {
+		console.log('New client connected!');
+		client.send('Hello new client!');
+	})
+	ws.on('message', (message) => {
+		if (message == 'DIE'){
+			console.log('SOMEONE KILLED ME!!');
+			ws.close();
+		}
+		console.log(`Received message => ${message}`);
+	});
+	//Close server after first disc
+	ws.on('close', () => {
+		console.log('Some Client Closed Connection!');
+	});
+	ws.on('error', (err) => {
+	console.log(`WebSocket Error: ${err.message.toString()}`)
+	})
 });
+
+wss.on('close', () => {
+	console.log('Server Closed!!');
+})
 
 //Closing server in 30 secs
 
-setTimeout(() => {
-  wss.close((err) => {
-	console.log(`Closing WsServer Error: ${err}`)
-  })
-}, 30000)
